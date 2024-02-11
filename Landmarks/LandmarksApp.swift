f56293ce4b66b6aa6a3737896ff19cdb602775ca
DumplingSociety <jobs4ever@me.com>
2024-02-11 16:40:17 -0600

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Xiangwei Li on 2/11/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
